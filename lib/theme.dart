cd25682c71d15243b0a51c88956a63062c4e5ae1
realsst1 <s@Nuchotu1>
2019-06-06 21:09:58 +0530

    color changed

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color accentColor=const Color(0xFFf08f8f);
-const Color lightAccentColor=const Color(0xFFFFAfAf);
-const Color darkAccentColor=const Color(0xFFD06F6F);
+const Color accentColor=const Color(0xFF6267ea);
+const Color lightAccentColor=const Color(0xFF7c7fdd);
+const Color darkAccentColor=const Color(0xFF373ddd);
